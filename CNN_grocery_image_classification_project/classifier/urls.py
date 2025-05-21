3ebb7c7d22d174f6a74b51dfc264d4a34f3544ee
nirmal-rawal <nirmalrawal3289@gmail.com>
2025-05-21 15:52:52 +0545

    Initial commit with CNN_grocery_image_classification_project

---
+++ CNN_grocery_image_classification_project/classifier/urls.py
@@ -0,0 +1,10 @@
+# from django.contrib import admin
+# from django.urls import path
+# from classifier.views import upload_image
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', upload_image, name='upload'),
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
